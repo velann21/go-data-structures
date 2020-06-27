df71952413b5de4b2c5916b7d32d6db492afc7b0
velann21 <velann21@gmail.com>
2020-06-27 20:32:49 +0200

    Feature/datastructure (#9)
    * Added OCP struct
    
    * Added the concat and merge LL code
    
    * added design patterns and form rect from coords
    
    * Added the git ignore
    
    * Added the git ignore
    
    * Implemented the circler LL
    
    * Implemnetd the concurrency with basic g rout
    
    * Implemneted the doubly linked list
    
    * Added display func in Doubly LL
    
    * Cleaned up
    
    * Added the find middle element function
    
    * Added the intersecting point
    
    * Implemeneted the algo of day and day2 in class
    
    Co-authored-by: velann21 <velan@reynencourt.com>

---
+++ dsa_class/day1/movezeros.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+//problemArry := [0,1,3,4,0]
+//ans: [1,3,4,0,0]
+
+func main()  {
+	fmt.Println(moveZeros([]int{0,1,3,4,0}))
+}
+
+func moveZeros(input []int)[]int{
+	count := 0
+	zerosCount := 0
+	nonZeroCount := 0
+	if input == nil{
+		return nil
+	}
+	//Iterate the input array
+    for _, v := range input{
+    	//If the element is not 0 then move the element to first index and increase the nonZeroCount++ && count++
+    	if v != 0{
+    		input[count] = v
+			count = count+1
+			nonZeroCount += 1
+			//Increase the zerosCount++ so that atlast we could move the number of zeros to one side
+		}  else{
+			zerosCount = zerosCount+1
+		}
+	}
+    for nonZeroCount < len(input)-1{
+		input[nonZeroCount] = 0
+		nonZeroCount = nonZeroCount+1
+	}
+
+    return input
+}
---
+++ dsa_class/day2/duplicates_triplets.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//input : [1,3,3,1,3]
+//output : [6]
+
+func main()  {
+	fmt.Println(duplicates_triplets([]int{1,5,2,1,5}))
+}
+
+func duplicates_triplets(input []int)int{
+	temp := 0
+	for k:=0; k<len(input); k++{
+	  temp ^= input[k]
+	}
+	return temp
+}
+
+

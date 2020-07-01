e2ba71579bf6499dd977b2bc72f96cb9f8abc0d7
velann21 <velan@reynencourt.com>
2020-07-01 08:42:27 +0200

    Implemenetd the binary with queue

--- trees/binarytree_queue.go
+++ trees/binarytree_queue.go
@@ -12,24 +12,37 @@ type TreeNode struct {
 
 type TreeNodeImpl struct {
 	Root *TreeNode
+	q Queue
 }
 
 func (t *TreeNodeImpl) Insert(data int){
 	if t.Root == nil{
-
+		t.Root = &TreeNode{Data:data, LeftNode:nil, RightNode:nil}
+		t.q.Push(t.Root)
+		return
 	}
+	node := t.q.Pop()
+	if node.LeftNode == nil{
+		node.LeftNode = &TreeNode{Data:data, LeftNode:nil, RightNode:nil}
+		t.q.Push(node)
+		t.q.Push(node.LeftNode)
+	}else {
+		node.RightNode = &TreeNode{Data:data, LeftNode:nil, RightNode:nil}
+		t.q.Push(node.RightNode)
+	}
+
 }
 
 
 type Queue struct {
-	que []int
+	que []*TreeNode
 }
 
-func (q *Queue) Push(data int){
+func (q *Queue) Push(data *TreeNode){
 	q.que = append(q.que, data)
 }
 
-func (q *Queue) Pop()(int){
+func (q *Queue) Pop()(*TreeNode){
 	data := q.que[0]
 	q.que = q.que[1:len(q.que)-1]
 	return data

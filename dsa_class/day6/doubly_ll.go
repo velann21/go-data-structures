adbf2cbf704a3f977867934d423f7d449af1fd7d
velann21 <velann21@gmail.com>
2020-07-12 08:10:42 +0200

    Feature/datastructure (#19)
    * Added OCP struct
    
    * Added the concat and merge LL code
    
    * added design patterns and form rect from coords
    
    * Added the git ignore
    
    * Added the git ignore
    
    * Implemented the circler LL
    
    * Implemnetd the concurrency with basic g rout
    
    * Implemneted the doubly linked list
    
    * Added display func in Doubly LL
    
    * Cleaned up
    
    * Added the find middle element function
    
    * Added the intersecting point
    
    * Implemeneted the algo of day and day2 in class
    
    * Solved the day2 problem
    
    * Day3 problem's
    
    * Added the stack impl using an array
    
    * Implemenetd the binary with queue
    
    * Implemenetd the binary with queue
    
    * Added the insert before func
    
    * Added the tree impl to create tree
    
    * Added the preorder traversal function
    
    * Added the inorder and preorder recursive adn preorder iterative function
    
    * Added the merge linked list code
    
    * Implemenetd the level order
    
    * Detect the loop in LL done
    
    * Done day5 problems
    
    * Day6.1
    
    Co-authored-by: velann21 <velan@reynencourt.com>

---
+++ dsa_class/day6/doubly_ll.go
@@ -0,0 +1,123 @@
+package main
+
+func main() {
+	node := NodeStruct{}
+	node.Insert(10)
+	node.Insert(20)
+	node.Insert(30)
+	node.Insert(40)
+	node.Insert(50)
+
+	node.AddToHead(05)
+	node.AddBefore(20, 15)
+	node.AddAfter(20, 25)
+
+	node.RemoveHead()
+	node.RemoveEnd()
+}
+
+type Node struct {
+	Data int
+	Next *Node
+	Prev *Node
+}
+
+type NodeStruct struct {
+	Head *Node
+}
+
+func (node *NodeStruct) Insert(data int){
+
+	if node.Head == nil{
+		node.Head = &Node{Data:data, Next:nil, Prev:nil}
+		return
+	}
+
+	last := node.Head
+
+	for last.Next != nil{
+		last = last.Next
+	}
+
+	last.Next = &Node{Data:data, Next:nil, Prev:last}
+	return
+}
+
+func (node *NodeStruct) AddToHead(data int){
+	if node.Head == nil{
+		node.Head = &Node{Data:data, Next:nil, Prev:nil}
+		return
+	}
+	temp := node.Head
+	node.Head = &Node{Data:data, Next:temp, Prev:nil}
+	temp.Prev = node.Head
+	return
+}
+
+func (node *NodeStruct) AddBefore(data1 int, data2 int){
+	if node.Head == nil{
+		return
+	}
+
+	if node.Head.Data == data1{
+		node.AddToHead(data2)
+	}
+
+	last := node.Head
+	for last != nil{
+		last = last.Next
+		if last.Data == data1{
+			tempprev := last.Prev
+			newNode := &Node{Data:data2, Next:last, Prev:tempprev}
+			tempprev.Next = newNode
+			last.Prev = newNode
+			return
+		}
+
+	}
+}
+
+
+func (node *NodeStruct) AddAfter(data1 int, data2 int){
+	if node.Head == nil{
+		return
+	}
+
+	last := node.Head
+	for last != nil{
+		last = last.Next
+		if last.Data == data1{
+			tempprev := last.Next
+			newNode := &Node{Data:data2, Next:tempprev, Prev:last}
+			tempprev.Prev = newNode
+			last.Next = newNode
+			break
+		}
+
+	}
+}
+
+func (node *NodeStruct) RemoveHead(){
+	if node.Head == nil{
+		return
+	}
+	temp := node.Head
+	if temp.Next != nil{
+		node.Head = temp.Next
+	}
+}
+
+func (node *NodeStruct) RemoveEnd(){
+	if node.Head == nil{
+		return
+	}
+	last := node.Head
+	for last.Next != nil{
+		last = last.Next
+	}
+
+	temp := last
+	temp.Next = nil
+	last = nil
+}
+

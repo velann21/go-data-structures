7262bbaf0ad120ec89bca1b46536242c6daea523
velann21 <velann21@gmail.com>
2020-07-12 07:12:03 +0200

    Feature/datastructure (#18)
    * Added OCP struct
    
    * Added the concat and merge LL code
    
    * added design patterns and form rect from coords
    
    * Added the git ignore
    
    * Added the git ignore
    
    * Implemented the circler LL
    
    * Implemnetd the concurrency with basic g rout
    
    * Implemneted the doubly linked list
    
    * Added display func in Doubly LL
    
    * Cleaned up
    
    * Added the find middle element function
    
    * Added the intersecting point
    
    * Implemeneted the algo of day and day2 in class
    
    * Solved the day2 problem
    
    * Day3 problem's
    
    * Added the stack impl using an array
    
    * Implemenetd the binary with queue
    
    * Implemenetd the binary with queue
    
    * Added the insert before func
    
    * Added the tree impl to create tree
    
    * Added the preorder traversal function
    
    * Added the inorder and preorder recursive adn preorder iterative function
    
    * Added the merge linked list code
    
    * Implemenetd the level order
    
    * Detect the loop in LL done
    
    * Done day5 problems
    
    Co-authored-by: velann21 <velan@reynencourt.com>

---
+++ dsa_class/day5/find_kth_element.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func main() {
+	node := NodeStruct19{}
+	node.Insert(10)
+	node.Insert(20)
+	node.Insert(30)
+	node.Insert(40)
+	node.Insert(50)
+	node.Insert(60)
+	node.Insert(70)
+	node.Insert(80)
+	node.Insert(90)
+	node.Insert(100)
+
+	node.FindKthElement()
+}
+
+type Node19 struct {
+	Data int
+	Next *Node19
+}
+
+type NodeStruct19 struct {
+	Root *Node19
+}
+
+func (node *NodeStruct19) Insert(data int){
+	if node.Root == nil{
+		node.Root = &Node19{Data:data, Next:nil}
+		return
+	}
+	last := node.Root
+	for last.Next != nil{
+		last = last.Next
+	}
+	last.Next = &Node19{Data:data, Next:nil}
+}
+
+func (node *NodeStruct19) FindKthElement(){
+	if node.Root == nil{
+		return
+	}
+
+	last := node.Root
+	fast := node.Root
+	fast = movektime(fast, 5)
+	for fast != nil{
+		fast = fast.Next
+		last = last.Next
+	}
+	fmt.Print(last.Data)
+}
+
+func movektime(root *Node19, moves int) *Node19{
+	counter := 0
+	for root != nil && counter < moves{
+		root = root.Next
+		counter ++
+	}
+	return root
+}

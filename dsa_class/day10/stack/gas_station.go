7783219ca06d31fd9b08d204f8bc9f46903b7858
velann21 <velann21@gmail.com>
2020-07-31 09:48:37 +0200

    Feature/dsa (#29)
    * Added OCP struct
    
    * Added the concat and merge LL code
    
    * added design patterns and form rect from coords
    
    * Added the git ignore
    
    * Added the git ignore
    
    * Implemented the circler LL
    
    * Implemnetd the concurrency with basic g rout
    
    * Implemneted the doubly linked list
    
    * Added display func in Doubly LL
    
    * Cleaned up
    
    * Added the find middle element function
    
    * Added the intersecting point
    
    * Implemeneted the algo of day and day2 in class
    
    * Solved the day2 problem
    
    * Day3 problem's
    
    * Added the stack impl using an array
    
    * Implemenetd the binary with queue
    
    * Implemenetd the binary with queue
    
    * Added the insert before func
    
    * Added the tree impl to create tree
    
    * Added the preorder traversal function
    
    * Added the inorder and preorder recursive adn preorder iterative function
    
    * Added the merge linked list code
    
    * Implemenetd the level order
    
    * Detect the loop in LL done
    
    * Done day5 problems
    
    * Day6.1
    
    * Completed the doubly LL problems
    
    * Added the helper func to get the host ips
    
    * Added the code to delete nth node from end
    
    * Added the funnction for template basics
    
    * Implemeted the code for stack and queue using array and LL
    
    * Implemented the get min value, removed node, add node  in O(1) using stack
    
    * Implemented the largest rectangle in hostogram problem
    
    * Added the rain water trapping problem
    
    * Organized well
    
    * Added the code for sort using stack and create new trr
    
    * Impl the gas station problem
    
    * WIP:tictokteo
    
    * Implemeted the code for find_next_greteast element
    
    * Added te code for queue using stack
    
    * Added indendation
    
    Co-authored-by: velann21 <velan@reynencourt.com>

--- dsa_class/day10/stack/gas_station.go
+++ dsa_class/day10/stack/gas_station.go
@@ -1,17 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+    gas := []int{1,2,3,4,5}
+	cost := []int{3,4,5,1,2}
+	result := CanCircleBackToStart(gas, cost)
+	fmt.Println(result)
 }
 
-func CanCircleBack(gas []int, cost []int)int{
+func CanCircleBackToStart(gas []int, cost []int)int{
 	gasRem := 0
 	costRem := 0
 	start := 0
 	tankRem := 0
 	for k, v:=range gas{
-		gasRem =+ v
-		costRem =+ cost[k]
+		gasRem += v
+		costRem += cost[k]
 		tankRem = gasRem - costRem
 		if tankRem < 0{
 			tankRem = 0

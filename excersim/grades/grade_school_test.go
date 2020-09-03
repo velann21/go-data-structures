9244682fba2c5682892f05f55c0d5b0d81b6a2a7
velann21 <velann21@gmail.com>
2020-09-03 08:38:47 +0200

    Feature/dsa (#46)
    * Added OCP struct
    
    * Added the concat and merge LL code
    
    * added design patterns and form rect from coords
    
    * Added the git ignore
    
    * Added the git ignore
    
    * Implemented the circler LL
    
    * Implemnetd the concurrency with basic g rout
    
    * Implemneted the doubly linked list
    
    * Added display func in Doubly LL
    
    * Cleaned up
    
    * Added the find middle element function
    
    * Added the intersecting point
    
    * Implemeneted the algo of day and day2 in class
    
    * Solved the day2 problem
    
    * Day3 problem's
    
    * Added the stack impl using an array
    
    * Implemenetd the binary with queue
    
    * Implemenetd the binary with queue
    
    * Added the insert before func
    
    * Added the tree impl to create tree
    
    * Added the preorder traversal function
    
    * Added the inorder and preorder recursive adn preorder iterative function
    
    * Added the merge linked list code
    
    * Implemenetd the level order
    
    * Detect the loop in LL done
    
    * Done day5 problems
    
    * Day6.1
    
    * Completed the doubly LL problems
    
    * Added the helper func to get the host ips
    
    * Added the code to delete nth node from end
    
    * Added the funnction for template basics
    
    * Implemeted the code for stack and queue using array and LL
    
    * Implemented the get min value, removed node, add node  in O(1) using stack
    
    * Implemented the largest rectangle in hostogram problem
    
    * Added the rain water trapping problem
    
    * Organized well
    
    * Added the code for sort using stack and create new trr
    
    * Impl the gas station problem
    
    * WIP:tictokteo
    
    * Implemeted the code for find_next_greteast element
    
    * Added te code for queue using stack
    
    * Added indendation
    
    * Implemented code for binary tree
    
    * Added the code for count leaf, full, nodes
    
    * Added the code for Binary serach tree and preorder traversal without recursion
    
    * Implemented the code for zigzag traverser
    
    * added the code inorder traverser without rec
    
    * Implemented the code for Cound half leaf nodes
    
    * Added the impl for tree height, print kth node
    
    * Added the code for LCS
    
    * Added the code for ismirror tree
    
    * Implemented the code for is two tree common
    
    * Added the first non rep char
    
    * Added the adjancency matric impl for graph
    
    * Added the code for graphs gen
    
    * Added the graph with matric and BFS for traversing
    
    * Added the code for DFS
    
    * Added the glovo test code
    
    * Added impl for grade school in excersim
    
    Co-authored-by: velann21 <velan@reynencourt.com>

---
+++ excersim/grades/grade_school_test.go
@@ -0,0 +1,187 @@
+
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+
+//func list(e []Grade) (s string) {
+//	for _, l := range e {
+//		s += fmt.Sprintln(l)
+//	}
+//	return s
+//}
+//
+func TestAddStudent(t *testing.T) {
+	school := NewSchool("SchoolA")
+	school.SetStudent()
+	student := "jim"
+	err := school.AddStudent(2, student)
+	if err != nil{
+		log.Fatal("Error should be empty")
+	}
+	fmt.Println("Student added")
+}
+
+
+//
+//func TestAddMoreSameGrade(t *testing.T) {
+//	exp := list([]Grade{{2, []string{"Blair James Paul"}}})
+//	s := New()
+//	s.Add("Blair", 2)
+//	s.Add("James", 2)
+//	s.Add("Paul", 2)
+//	got := list(s.Enrollment())
+//	if got != exp {
+//		t.Errorf(`Add more same grade, got
+//%sexpected:
+//%s`, got, exp)
+//	}
+//}
+//
+//func TestAddDifferentGrades(t *testing.T) {
+//	exp := list([]Grade{
+//		{3, []string{"Chelsea"}},
+//		{7, []string{"Logan"}},
+//	})
+//	s := New()
+//	s.Add("Chelsea", 3)
+//	s.Add("Logan", 7)
+//	got := list(s.Enrollment())
+//	if got != exp {
+//		t.Errorf(`Add different grades, got
+//%sexpected:
+//%s`, got, exp)
+//	}
+//}
+//
+func TestGetGrade(t *testing.T) {
+	school := NewSchool("SchoolA")
+	school.SetStudent()
+	student := "jim"
+	err := school.AddStudent(2, student)
+	if err != nil {
+		log.Fatal("Error should be empty")
+	}
+	fmt.Println("Student added")
+	students, err := school.ListStudent(2)
+	if err != nil {
+		log.Fatal("Error should be empty")
+	}
+	fmt.Println(students)
+
+}
+
+func TestSchoolA_ListAllGradeStudentsStudents(t *testing.T) {
+	school := NewSchool("SchoolA")
+	school.SetStudent()
+	student := "jim"
+	student1 := "velan"
+	student2 := "Apple"
+	student3 := "Banana"
+	err := school.AddStudent(2, student)
+	if err != nil {
+		log.Fatal("Error should be empty")
+	}
+	err = school.AddStudent(2, student1)
+	if err != nil {
+		log.Fatal("Error should be empty")
+	}
+	err = school.AddStudent(1, student2)
+	if err != nil {
+		log.Fatal("Error should be empty")
+	}
+	err = school.AddStudent(1, student3)
+	if err != nil {
+		log.Fatal("Error should be empty")
+	}
+	studs, err := school.ListAllGradeStudents()
+	if err != nil{
+
+	}
+	fmt.Println(studs)
+
+}
+//
+//func TestNonExistantGrade(t *testing.T) {
+//	s := New()
+//	got := s.Grade(1)
+//	if len(got) != 0 {
+//		t.Errorf(`Get non-existent grade, got
+//%q
+//expected
+//[]`, got)
+//	}
+//}
+//
+//func TestSortedEnrollment(t *testing.T) {
+//	exp := list([]Grade{
+//		{3, []string{"Kyle"}},
+//		{4, []string{"Christopher Jennifer"}},
+//		{6, []string{"Kareem"}},
+//	})
+//	s := New()
+//	s.Add("Jennifer", 4)
+//	s.Add("Kareem", 6)
+//	s.Add("Christopher", 4)
+//	s.Add("Kyle", 3)
+//	got := list(s.Enrollment())
+//	if got != exp {
+//		t.Errorf(`Sorted enrollment, got
+//%sexpected:
+//%s`, got, exp)
+//	}
+//}
+//
+//const (
+//	minLevel   = 1
+//	maxLevel   = 9
+//	enrollment = 400
+//)
+//
+//func BenchmarkAddStudents(b *testing.B) {
+//	const pool = 1e6 // pool of students
+//	names := make([]string, pool)
+//	levels := make([]int, pool)
+//	for i := range names {
+//		names[i] = strconv.Itoa(rand.Intn(1e5))
+//		levels[i] = minLevel + rand.Intn(maxLevel-minLevel+1)
+//	}
+//	p := 0
+//	b.ResetTimer()
+//	for i := 0; i < b.N; i++ {
+//		// bench combined time to create a school and add
+//		// a number of students, drawn from a pool of students
+//		s := New()
+//		for t := 0; t < enrollment; t++ {
+//			s.Add(names[p], levels[p])
+//			p = (p + 1) % pool
+//		}
+//	}
+//}
+//
+//func BenchmarkEnrollment(b *testing.B) {
+//	const pool = 1000 // pool of schools
+//	ss := make([]*School, pool)
+//	for i := range ss {
+//		s := New()
+//		for t := 0; t < enrollment; t++ {
+//			s.Add(
+//				strconv.Itoa(rand.Intn(1e5)),
+//				minLevel+rand.Intn(maxLevel-minLevel+1))
+//		}
+//		ss[i] = s
+//	}
+//	p := 0
+//	b.ResetTimer()
+//	for i := 0; i < b.N; i++ {
+//		// bench time to get enrollment of a full school,
+//		// averaged over a pool of schools.
+//		ss[p].Enrollment()
+//		p = (p + 1) % pool
+//	}
+//}

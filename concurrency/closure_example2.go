ef825b7fe159b237d3b2827b6f25d52397fa65cf
velann21 <velan@reynencourt.com>
2020-11-12 09:52:57 +0100

    Added the goroutine closure function

---
+++ concurrency/closure_example2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	wg := sync.WaitGroup{}
+	for i:=0; i<=3; i++{
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			fmt.Println("I is  :",i)
+		}(i)
+	}
+
+	wg.Wait()
+
+
+}

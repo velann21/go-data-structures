2db9301f208b70aee1d7016df8f56988ffc9b7b2
velann21 <velann21@gmail.com>
2020-11-20 22:22:35 +0100

    Feature/concurrency (#59)
    * Added OCP struct
    
    * Added the concat and merge LL code
    
    * added design patterns and form rect from coords
    
    * Added the git ignore
    
    * Added the git ignore
    
    * Implemented the circler LL
    
    * Implemnetd the concurrency with basic g rout
    
    * Implemneted the doubly linked list
    
    * Added display func in Doubly LL
    
    * Cleaned up
    
    * Added the find middle element function
    
    * Added the intersecting point
    
    * Implemeneted the algo of day and day2 in class
    
    * Solved the day2 problem
    
    * Day3 problem's
    
    * Added the stack impl using an array
    
    * Implemenetd the binary with queue
    
    * Implemenetd the binary with queue
    
    * Added the insert before func
    
    * Added the tree impl to create tree
    
    * Added the preorder traversal function
    
    * Added the inorder and preorder recursive adn preorder iterative function
    
    * Added the merge linked list code
    
    * Implemenetd the level order
    
    * Detect the loop in LL done
    
    * Done day5 problems
    
    * Day6.1
    
    * Completed the doubly LL problems
    
    * Added the helper func to get the host ips
    
    * Added the code to delete nth node from end
    
    * Added the funnction for template basics
    
    * Implemeted the code for stack and queue using array and LL
    
    * Implemented the get min value, removed node, add node  in O(1) using stack
    
    * Implemented the largest rectangle in hostogram problem
    
    * Added the rain water trapping problem
    
    * Organized well
    
    * Added the code for sort using stack and create new trr
    
    * Impl the gas station problem
    
    * WIP:tictokteo
    
    * Implemeted the code for find_next_greteast element
    
    * Added te code for queue using stack
    
    * Added indendation
    
    * Implemented code for binary tree
    
    * Added the code for count leaf, full, nodes
    
    * Added the code for Binary serach tree and preorder traversal without recursion
    
    * Implemented the code for zigzag traverser
    
    * added the code inorder traverser without rec
    
    * Implemented the code for Cound half leaf nodes
    
    * Added the impl for tree height, print kth node
    
    * Added the code for LCS
    
    * Added the code for ismirror tree
    
    * Implemented the code for is two tree common
    
    * Added the first non rep char
    
    * Added the adjancency matric impl for graph
    
    * Added the code for graphs gen
    
    * Added the graph with matric and BFS for traversing
    
    * Added the code for DFS
    
    * Added the glovo test code
    
    * Added impl for grade school in excersim
    
    * Added the example helm charts
    
    * added the k8s libs
    
    * Added the recursive functions
    
    * Added the ticker examples
    
    * Modified the gas station problem
    
    * Implemeneytd the circular queue
    
    * Added go templating
    
    * Added the recursion to cal avr age from JSON
    
    * Implemeneted the go concurrency code
    
    * Added the goroutine closure function
    
    * Added more logs
    
    * Implemneted the concurrency with channels
    
    * Added the code for select block in golang
    
    * Adde dthe code with select block with default blk
    
    * Added the code with select block with timeout
    
    * Create the select with non blocking chn
    
    Co-authored-by: velann21 <velan@reynencourt.com>

---
+++ concurrency/select_nonblocking.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	ch1 := make(chan string, 1)
+	go func(){
+		for i:= 0; i<=2; i++{
+			ch1 <- "Message"
+		}
+	}()
+
+
+	for{
+		select{
+		case ch1Rcv := <- ch1:
+			fmt.Println(ch1Rcv)
+		default:
+			fmt.Println("It is default block no message")
+			time.Sleep(2 * time.Second)
+		}
+		fmt.Println("Processing .....")
+		time.Sleep(1500* time.Millisecond)
+	}
+
+}

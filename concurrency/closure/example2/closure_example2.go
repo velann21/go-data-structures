3642ef32f5b503837548f1bb8dc188195c9f9e0a
singaravelan <singaravelan.nandhakumar@ingka.ikea.com>
2021-03-08 07:39:16 +0100

    Added the wg and closure

--- concurrency/closure/example2/closure_example2.go
+++ concurrency/closure/example2/closure_example2.go
@@ -10,10 +10,10 @@ func main() {
 	wg := sync.WaitGroup{}
 	for i:=0; i<=3; i++{
 		wg.Add(1)
-		go func(i int) {
+		go func() {
 			defer wg.Done()
 			fmt.Println("I is  :",i)
-		}(i)
+		}()
 	}
 
 	wg.Wait()

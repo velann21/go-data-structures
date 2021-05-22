14575861a6200eb15993dbce090b5574ea5abea7
velann21 <velann21@gmail.com>
2021-05-22 11:37:24 +0200

    Added the array adt code

--- data_structure/array/array_adt.go
+++ data_structure/array/array_adt.go
@@ -95,6 +95,166 @@ func (al *ArrayList) BinarySearch(element int)int{
 	return 0
 }
 
+func (al *ArrayList) Get(index int)int{
+	if index < 0 || index > al.Length{
+		return 0
+	}
+	return al.Array[index]
+}
+
+func (al *ArrayList) Set(index, element int){
+	if index < 0 || index > al.Length{
+		return
+	}
+	al.Array[index] = element
+}
+
+func (al *ArrayList) FindMaxMin()(int, int){
+	if al.Length <=0 {
+		return 0, 0
+	}
+	max := al.Array[0]
+	min := al.Array[0]
+	for _, value := range al.Array{
+		if value > max{
+			max = value
+		}else if value < min{
+			min = value
+		}
+	}
+	return max, min
+}
+
+func (al *ArrayList) AddFunc()int{
+	if al.Length <=0 {
+		return 0
+	}
+	sum := 0
+	for _, value := range al.Array{
+		sum += value
+	}
+	return sum
+}
+
+func (al *ArrayList) CalculateAvg()int{
+	if al.Length <=0 {
+		return 0
+	}
+	avg := 0
+	for _, value := range al.Array{
+		avg += value
+	}
+	avg = avg/al.Length
+	return avg
+}
+
+func (al *ArrayList) LeftShift()[]int{
+	if al.Length <=0 && al.Length == 1 {
+		return al.Array
+	}
+	for i:=0; i<=al.Length; i++{
+		if i+1 <= al.Length{
+			al.Array[i] = al.Array[i+1]
+		}
+	}
+	return al.Array
+}
+
+func (al *ArrayList) ReverseArray()[]int{
+	if al.Length <=0 && al.Length == 1 {
+		return al.Array
+	}
+	start := 0
+	end := al.Length
+	for end > start{
+		temp := al.Array[start]
+		al.Array[start] = al.Array[end]
+		al.Array[end] = temp
+	}
+	return al.Array
+}
+
+func (al *ArrayList) LeftRotate(rotateNumber int)[]int{
+	if al.Length <=0 && al.Length == 1 {
+		return al.Array
+	}
+	if rotateNumber == 1{
+		tempVar := al.Array[0]
+		al.Rotate()
+		al.Array[al.Length] = tempVar
+	}
+	return al.Array
+}
+
+func (al *ArrayList) Rotate(){
+	for i:=0; i<=al.Length; i++ {
+		if i+1 <= al.Length{
+			al.Array[i] = al.Array[i+1]
+		}
+	}
+}
+
+func (al *ArrayList) CheckArraySorted()bool{
+	if al.Length <=0 && al.Length == 1 {
+		return true
+	}
+	for i:=0; i<al.Length; i++{
+		if i+1 <=al.Length && al.Array[i] > al.Array[i+1]{
+			return false
+		}
+	}
+	return true
+}
+
+func (al *ArrayList) MergeKSortedArrays(array1, array2 []int)[]int{
+	if len(array1) <=0 && len(array2) > 0{
+		return array2
+	}
+	if len(array2) <=0 && len(array1) > 0{
+		return array1
+	}
+	if len(array2) <=0 && len(array1)<=0{
+		return nil
+	}
+
+	arrayOnePointer := 0
+	arrayTwoPointer := 0
+	auxiliaryArray := make([]int, 0)
+	auxiliaryArrayPointer := 0
+
+
+	for arrayOnePointer<=len(array1) && arrayTwoPointer<=len(array2) {
+		if array1[arrayOnePointer] < array2[arrayTwoPointer]{
+			auxiliaryArray[auxiliaryArrayPointer] = array1[arrayOnePointer]
+			arrayOnePointer ++
+			auxiliaryArrayPointer++
+		}else if array1[arrayOnePointer] > array2[arrayTwoPointer] {
+			auxiliaryArray[auxiliaryArrayPointer] = array2[arrayTwoPointer]
+			arrayTwoPointer ++
+			auxiliaryArrayPointer++
+		}else if array1[arrayOnePointer] == array2[arrayTwoPointer] {
+			auxiliaryArray[auxiliaryArrayPointer] = array2[arrayTwoPointer]
+			arrayTwoPointer ++
+			arrayOnePointer ++
+			auxiliaryArrayPointer++
+		}
+	}
+
+	if arrayOnePointer < len(array1){
+		for i := arrayOnePointer; i<= len(array1); i++{
+			auxiliaryArray[auxiliaryArrayPointer] = array1[i]
+			auxiliaryArrayPointer++
+		}
+	}else if arrayTwoPointer < len(array2){
+		for i := arrayTwoPointer; i<= len(array2); i++{
+			auxiliaryArray[auxiliaryArrayPointer] = array2[i]
+			auxiliaryArrayPointer++
+		}
+	}
+	return auxiliaryArray
+}
+
+
 func main() {
 	list := NewArrayList(2)
 	for i:=0; i<=20; i++{
@@ -116,6 +276,7 @@ func main() {
 		return
 	}
 	fmt.Println("Found")
+
 }
 
 
